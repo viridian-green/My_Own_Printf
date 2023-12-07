c7c0b65a5e05ef679a2a0fb1a9fedc3bc9cd58d0
moldypeaches <adelejulietmartin@gmail.com>
2023-12-07 14:52:21 +0100

    ok

--- libftprintf.h
+++ libftprintf.h
@@ -1,5 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademarti <adelemartin@student.42.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 15:07:02 by ademarti          #+#    #+#             */
+/*   Updated: 2023/12/07 14:45:05 by ademarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
+
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft.h"
+
+int	ft_printf(const char *, ...);
 
 #endif
--- printf.c
+++ printf.c
@@ -1,4 +1,30 @@
-int    ft_printf(const char *, ...)
-{
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademarti <adelemartin@student.42.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 13:40:31 by ademarti          #+#    #+#             */
+/*   Updated: 2023/12/07 14:29:56 by ademarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "libftprintf.h"
+
+int	ft_printf(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+
+	if (va_arg( args,  char * ))
+	if (va_arg( args,  char))
+	if (va_arg( args,  int)) //%d
+	if (va_arg( args,  int)) //%i
+	if (va_arg( args,  unsigned int)) //%u
+	if (va_arg( args,  void))
+	if (va_arg( args,  unsigned int)) //%x
+	if (va_arg( args,  unsigned int)) //%X
+	if (va_arg( args,  char)) //%% percent sign
+	va_end (args);
 }

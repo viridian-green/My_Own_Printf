029f02ac25a4a8b0281d791ba2baf14d83e2c54e
moldypeaches <adelejulietmartin@gmail.com>
2023-12-08 14:11:43 +0100

    ok

--- printf.c
+++ printf.c
@@ -6,13 +6,13 @@
 /*   By: ademarti <adelemartin@student.42.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 13:40:31 by ademarti          #+#    #+#             */
-/*   Updated: 2023/12/07 17:12:49 by ademarti         ###   ########.fr       */
+/*   Updated: 2023/12/08 14:11:32 by ademarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-int	writeformat(char *s, va_list args)
+int	writeformat(const char *s, va_list args)
 {
 	size_t	pos;
 
@@ -21,16 +21,18 @@ int	writeformat(char *s, va_list args)
 	{
 		if (s[pos] == '%')
 		{
-			if (s[pos + 1] == 'c' || s[pos + 1] == 's' || s[pos + 1] == 'p')
+			if (s[pos + 1] == 'c' || s[pos + 1] == 's' || s[pos + 1] == 'p' || s[pos + 1] == '%')
 			{
-				characters(args);
-				pos = pos + characters(args);
+				char *current_arg = va_arg(args, char *);
+				characters(current_arg);
+				pos = pos + characters(current_arg);
 			}
 			else if (s[pos + 1] == 'd' || s[pos + 1] == 'i' || s[pos + 1] == 'u'|| s[pos + 1] == 'x' ||
-			s[pos + 1] == 'X' || s[pos + 1] == '%')
+			s[pos + 1] == 'X')
 			{
-				numbers(args);
-				pos = pos + characters(args);
+				int current_arg = va_arg(args, int);
+				numbers(current_arg);
+				pos = pos + characters(current_arg);
 			}
 		}
 		else
@@ -47,12 +49,11 @@ int	ft_printf(const char *format, ...)
 	va_list	args;
 	va_start(args, format);
 	size_t	i;
+	i = 0;
 
-	//number of arguments
-
-	while (args[i] != 0)
+	while (i < format)
 	{
-		writeformat(format, args[i]);
+		const char *value = va_arg(args, char*);
 		i++;
 	}
 
@@ -61,7 +62,7 @@ int	ft_printf(const char *format, ...)
 	if (va_arg( args,  int)) //%d
 	if (va_arg( args,  int)) //%i
 	if (va_arg( args,  unsigned int)) //%u
-	if (va_arg( args,  void))
+	if (va_arg( args,  void *))
 	if (va_arg( args,  unsigned int)) //%x
 	if (va_arg( args,  unsigned int)) //%X
 	if (va_arg( args,  char)) //%% percent sign
